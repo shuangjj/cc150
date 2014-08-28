c946d095ee083fa7cb9468d6cfcd3985f67d42ba
shuang-ux32vd <co.liang.ol@gmail.com>
2014-08-28 15:53:23 -0400

    1-2

---
+++ 1-2.java
@@ -0,0 +1,16 @@
+class Test {
+    public static void main(String args[]) {
+        String str = "abcd";
+        System.out.println(reverse(str)); 
+    }
+    public static String reverse(String str) {
+        char[] strchars = str.toCharArray();
+        for(int i = 0, j = str.length()-1; i < j; i++, j--) {
+            char tmp = strchars[i];
+            strchars[i] = strchars[j];
+            strchars[j] = tmp;
+        }
+        return new String(strchars);
+    }
+    
+}
