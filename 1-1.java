d216118fa63937b76211de23ca38ec2f1759476b
shuang-ux32vd <co.liang.ol@gmail.com>
2014-08-28 15:26:48 -0400

    start crack the code interview

---
+++ 1-1.java
@@ -0,0 +1,29 @@
+
+class UniqChar {
+    public static void main(String args[]) {
+        String str = args[0]; 
+        if(isUnique(str)) {
+            System.out.println(str + " is unique");
+        } else {
+            System.out.println(str + " is not unique");
+        }
+    }
+    public static boolean isUnique(String str) {
+        int uppermap = 0;
+        int lowermap = 0;
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if( c >= 'a' && c <= 'z' ) {
+                int mask = 1 << (c - 'a');
+                if( (lowermap & mask) != 0 ) return false;
+                lowermap |= mask;
+            }
+            else if( c >= 'A' && c <= 'Z') {
+                int mask = 1 << (c - 'A');
+                if( (uppermap & mask) != 0 ) return false;
+                uppermap |= mask;
+            }
+        }
+        return true;
+    }
+}
